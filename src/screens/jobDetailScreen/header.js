3d49bf2120d8b49a45959a0958e58bd918703960
LaurenceM10 <laurenmontenegro10@gmail.com>
2021-05-27 22:10:21 -0600

    Extract header in to a new component

---
+++ src/screens/jobDetailScreen/header.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Image, Text, View, StyleSheet } from 'react-native';
+
+import SizedBox from 'components/shared/sizedBox';
+import Icon from 'react-native-vector-icons/Ionicons';
+import TextGradient from 'components/shared/textGradient';
+
+import { colors } from 'core/presentation/styles/theme';
+import { POPPINS } from 'core/presentation/styles/fonts';
+
+function Header(props) {
+  const { logoUrl, title, location, commitment } = props;
+
+  return (
+    <View style={styles.header}>
+      <Image
+        style={styles.logo}
+        source={{
+          uri: logoUrl,
+        }}
+      />
+      <SizedBox height={18} />
+      <TextGradient
+        style={styles.jobTitle}
+        containerStyles={styles.textGradientContainer}>
+        {title}
+      </TextGradient>
+      <SizedBox height={20} />
+      <View style={styles.labelContainer}>
+        <View
+          style={{
+            borderRadius: 12,
+            paddingVertical: 8,
+            paddingHorizontal: 18,
+            backgroundColor: colors.primaryColor,
+          }}>
+          <Text
+            style={{
+              fontFamily: POPPINS.light,
+              fontSize: 14,
+              color: '#FFF',
+            }}>
+            <Icon size={14} name={'time'} color="#FFF" />
+            <SizedBox width={6} />
+            {commitment}
+          </Text>
+        </View>
+        <View
+          style={{
+            borderRadius: 12,
+            paddingVertical: 8,
+            paddingHorizontal: 18,
+            backgroundColor: 'rgba(0, 123,255, .06)',
+          }}>
+          <Text
+            style={{
+              fontFamily: POPPINS.light,
+              fontSize: 14,
+              color: colors.primaryColor,
+            }}>
+            <Icon size={14} name={'location'} color={colors.primaryColor} />
+            <SizedBox width={6} />
+            {location}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    height: 250,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+  },
+  logo: {
+    width: 65,
+    height: 65,
+    backgroundColor: '#fff',
+    borderRadius: 14,
+  },
+  jobTitle: {
+    fontFamily: POPPINS.semibold,
+    fontSize: 18,
+    marginVertical: 4,
+  },
+  textGradientContainer: { textAlign: 'center', maxWidth: '80%' },
+  labelContainer: {
+    width: '75%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
+
+export default Header;
--- src/screens/jobDetailScreen/index.js
+++ src/screens/jobDetailScreen/index.js
@@ -1,106 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
-  Text,
-  View,
   StyleSheet,
   ScrollView,
-  Image,
   SafeAreaView,
   KeyboardAvoidingView,
 } from 'react-native';
 import Padding from 'components/shared/padding';
 import Section from 'components/shared/section';
-import SizedBox from 'components/shared/sizedBox';
-import TextGradient from 'components/shared/textGradient';
-import Icon from 'react-native-vector-icons/Ionicons';
-
-import { POPPINS } from 'core/presentation/styles/fonts';
-import { colors } from 'core/presentation/styles/theme';
+import Header from 'screens/jobDetailScreen/header';
 
 function JobDetailScreen() {
   return (
     <SafeAreaView style={styles.screen}>
       <KeyboardAvoidingView style={styles.container} behavior="height">
         <ScrollView>
-          <View
-            style={{
-              height: 250,
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderBottomWidth: 1,
-              borderBottomColor: '#eee',
-            }}>
-            <Image
-              style={{
-                width: 65,
-                height: 65,
-                backgroundColor: '#fff',
-                borderRadius: 14,
-              }}
-              source={{
-                uri: 'https://rotulosmatesanz.com/wp-content/uploads/2017/09/2000px-Google_G_Logo.svg_.png',
-              }}
-            />
-            <SizedBox height={18} />
-            <TextGradient
-              containerStyles={{ textAlign: 'center', maxWidth: '80%' }}
-              style={{
-                fontFamily: POPPINS.semibold,
-                fontSize: 18,
-                marginVertical: 4,
-              }}>
-              Senior Software Ingineer
-            </TextGradient>
-
-            <SizedBox height={20} />
-            <View
-              style={{
-                width: '75%',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-              }}>
-              <View
-                style={{
-                  borderRadius: 12,
-                  paddingVertical: 8,
-                  paddingHorizontal: 18,
-                  backgroundColor: colors.primaryColor,
-                }}>
-                <Text
-                  style={{
-                    fontFamily: POPPINS.light,
-                    fontSize: 14,
-                    color: '#FFF',
-                  }}>
-                  <Icon size={14} name={'time'} color="#FFF" />
-                  <SizedBox width={6} />
-                  Full Time
-                </Text>
-              </View>
-              <View
-                style={{
-                  borderRadius: 12,
-                  paddingVertical: 8,
-                  paddingHorizontal: 18,
-                  backgroundColor: 'rgba(0, 123,255, .06)',
-                }}>
-                <Text
-                  style={{
-                    fontFamily: POPPINS.light,
-                    fontSize: 14,
-                    color: colors.primaryColor,
-                  }}>
-                  <Icon
-                    size={14}
-                    name={'location'}
-                    color={colors.primaryColor}
-                  />
-                  <SizedBox width={6} />
-                  San Francisco
-                </Text>
-              </View>
-            </View>
-          </View>
+          <Header />
           <Padding paddingHorizontal={16} paddingVertical={10}>
             <Section title="Overview" />
           </Padding>

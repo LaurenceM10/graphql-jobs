513a18a477c1c9c0413828f0fba577a75e1c308a
LaurenceM10 <laurenmontenegro10@gmail.com>
2021-06-01 20:19:20 -0600

    Minor change on test folder structure

--- __tests__/repositories/companies/companies.test.js
+++ __tests__/repositories/companies/companies.test.js
@@ -1,33 +1,9 @@
 import React from 'react';
 import { MockedProvider } from '@apollo/client/testing';
 import { renderHook } from '@testing-library/react-hooks';
-import { usePopularCompanies } from './index';
-import { COMPANY_LIST_QUERY } from './gql';
-
-const QueryResponse = {
-  data: {
-    companies: [
-      {
-        id: 'cjtu8etmr001n0824o1v2qvgr',
-        name: 'Apollo',
-        slug: 'apollo',
-        logoUrl: 'https://cdn.filestackcontent.com/uRGQ5QfTT8mforGeyUS5',
-      },
-      {
-        id: 'cjuv4wxdc00dl0735ts4ukqs7',
-        name: 'Prisma',
-        slug: 'prisma',
-        logoUrl: 'https://cdn.filestackcontent.com/dZHmLkPRTFObRrqHsD7A',
-      },
-      {
-        id: 'cjuvawyjc015m07351qg6jo23',
-        name: 'Mixcloud',
-        slug: 'mixcloud',
-        logoUrl: 'https://cdn.filestackcontent.com/XeZED8N9QAG5BBxdyyL1',
-      },
-    ],
-  },
-};
+import { usePopularCompanies } from '../../../src/repositories/companies';
+import { COMPANY_LIST_QUERY } from '../../../src/repositories/companies/gql';
+import { QueryResponse } from '../data/responses';
 
 describe('companies', () => {
   describe('usePopularCompanies custom hook', () => {
---
+++ __tests__/repositories/data/responses.js
@@ -0,0 +1,24 @@
+export const QueryResponse = {
+  data: {
+    companies: [
+      {
+        id: 'cjtu8etmr001n0824o1v2qvgr',
+        name: 'Apollo',
+        slug: 'apollo',
+        logoUrl: 'https://cdn.filestackcontent.com/uRGQ5QfTT8mforGeyUS5',
+      },
+      {
+        id: 'cjuv4wxdc00dl0735ts4ukqs7',
+        name: 'Prisma',
+        slug: 'prisma',
+        logoUrl: 'https://cdn.filestackcontent.com/dZHmLkPRTFObRrqHsD7A',
+      },
+      {
+        id: 'cjuvawyjc015m07351qg6jo23',
+        name: 'Mixcloud',
+        slug: 'mixcloud',
+        logoUrl: 'https://cdn.filestackcontent.com/XeZED8N9QAG5BBxdyyL1',
+      },
+    ],
+  },
+};
